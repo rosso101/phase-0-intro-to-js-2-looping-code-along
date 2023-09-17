bb4d473c4701c49ae6addc80d91e77be44b45aec
Victor <victorprivv101@gmail.com>
2023-09-17 22:47:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const suprise = "suprise"
+const thankYou =[];
+
+function writeCards(names, surprise) {
+    for (let i = 0; i < names.lenth; i++){
+        const msg = `Thank you, ${names}, for the wonderful ${surprise} gift`;
+        thankYou.push(msg)
+        debugger;
+    }
+    return thankYou
+}
+function countDown() {
+    let count = 10
+    while(count >=0) {
+        console.log(count--)
+
+    }
+}
+
+countDown()
